7ed7d1b6b0d44907bd798bda242a761cc6123098
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-04-30 16:17:55 +0530

    import urls of product

--- Django-Projects-practice/myblog/myblog/urls.py
+++ Django-Projects-practice/myblog/myblog/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+# from . import views
+from product import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello),
+    # path('', views.hello),
 ]
