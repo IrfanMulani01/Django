f2ec1f849735ab55fd061a1e62db48f7ae845b73
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-01 15:51:32 +0530

    create new model name Product review it relate with model name product (it is one to many relationship)

--- Django-Projects-practice/myblog/product/models.py
+++ Django-Projects-practice/myblog/product/models.py
@@ -4,7 +4,21 @@ class product(models.Model):
     prod_name = models.CharField(max_length=100)
     prod_descri = models.CharField(max_length=200)
     price = models.IntegerField()
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.prod_name
+    
+
+
+        ######  one - to  - many relationship   ######
+
+class Prod_review(models.Model):
+    prod = models.ForeignKey(product, on_delete=models.CASCADE)
+    review = models.IntegerField()
+    com = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.prod
