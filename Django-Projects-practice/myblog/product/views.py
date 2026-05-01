2cfe65b5e87e78b2017fdd9362ca22498d1a752b
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-01 11:52:18 +0530

    create product list component

--- Django-Projects-practice/myblog/product/views.py
+++ Django-Projects-practice/myblog/product/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 def product(request):
     return HttpResponse("this is product page")
 
-# def product_list(request):
-#     return HttpResponse("this is product list page")
+def product_list(request):
+    return HttpResponse("this is product list page")
