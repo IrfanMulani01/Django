9dc92fbef45903254f7fd904861cdbf8b5cf2e2d
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-01 16:04:41 +0530

    register product to admin panel

--- Django-Projects-practice/myblog/product/admin.py
+++ Django-Projects-practice/myblog/product/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import product, Prod_review
 
 admin.site.register(product)
-admin.site.register(Prod_review)
+
+# admin.site.register(Prod_review)
--- Django-Projects-practice/myblog/product/models.py
+++ Django-Projects-practice/myblog/product/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class product(models.Model):
     prod_name = models.CharField(max_length=100)
-    prod_descri = models.CharField(max_length=200)
     price = models.IntegerField()
     image = models.ImageField(upload_to='products/', null=True, blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
@@ -21,4 +20,13 @@ class Prod_review(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.prod
+        return self.prod.prod_name
+    
+
+class Prod_Details(models.Model):
+    prod1 = models.ForeignKey(product, on_delete=models.CASCADE)
+    prod_des = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.prod1.prod_name
