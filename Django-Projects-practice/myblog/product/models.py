f298208e6be244746e6c4e83b6d2fbfdf4b5ef37
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-01 12:19:12 +0530

    file and create table name, description, price and create date

--- Django-Projects-practice/myblog/product/models.py
+++ Django-Projects-practice/myblog/product/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class product(models.model):
+    prod_name = models.CharField(max_length=100)
+    prod_descri = models.CharField(max_length=200)
+    price = models.IntegerField()
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.prod_name
