ad15d5e5052d8cf370e8d05e8ea15397fc20fc41
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-02 10:55:08 +0530

    register courses and students to admin pannel

--- Django-Projects-practice/myblog/product/admin.py
+++ Django-Projects-practice/myblog/product/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import product, Prod_review, User_profile
+from .models import product, Prod_review, User_profile, Course, Student
 
+
+# one to many relationship
 admin.site.register(product)
-
 admin.site.register(Prod_review)
 
+# one to one relationship
 admin.site.register(User_profile)
+
+
+# Many to many relationship
+admin.site.register(Course)
+admin.site.register(Student)
