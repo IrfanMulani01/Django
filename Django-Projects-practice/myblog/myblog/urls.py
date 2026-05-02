0ffbf398df6f3db37d88d1bea820f049ae551389
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-02 16:19:22 +0530

    set path of teacher app

--- Django-Projects-practice/myblog/myblog/urls.py
+++ Django-Projects-practice/myblog/myblog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-# from . import views
-from product import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', views.hello),
     path('product/', include('product.urls')),
+    path('teacher/', include('teacher.urls')),
+    
 ]
