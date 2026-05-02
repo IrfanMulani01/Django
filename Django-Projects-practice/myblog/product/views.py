5c2a37886ed46abb85a88f497854bf5b2fc1917f
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-02 15:32:50 +0530

    create three functions to display dynamic output

--- Django-Projects-practice/myblog/product/views.py
+++ Django-Projects-practice/myblog/product/views.py
@@ -10,3 +10,15 @@ def product_list(request):
 
 def prod_cat(request):
     return HttpResponse("this is product category page")
+
+
+def Student_detail(request, id):
+    return HttpResponse(f"student id is {id}")
+
+
+def product_detail(request, name):
+    return HttpResponse(f"product name {name}")
+
+
+def article_by_year(request, year):
+    return HttpResponse(f"year: {year}")
