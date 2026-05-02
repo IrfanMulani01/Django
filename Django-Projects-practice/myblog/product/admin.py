9c6654d92cdbac667dea38dce7a767c313e2f1db
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-02 10:48:11 +0530

    register User Profile to admin

--- Django-Projects-practice/myblog/product/admin.py
+++ Django-Projects-practice/myblog/product/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import product, Prod_review
+from .models import product, Prod_review, User_profile
 
 admin.site.register(product)
 
 admin.site.register(Prod_review)
+
+admin.site.register(User_profile)
