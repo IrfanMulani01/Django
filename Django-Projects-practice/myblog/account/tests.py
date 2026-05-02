95535fd3f5aaf04af7fd3b49be6a1012afd7baee
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-02 16:08:46 +0530

    comment it out

--- Django-Projects-practice/myblog/account/tests.py
+++ Django-Projects-practice/myblog/account/tests.py
@@ -1,13 +1,21 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.urls import reverse
 
-class UserModelTest(TestCase):
-    def test_create_user_and_check_name(self):
-        user = User.objects.create_user(
-            username='irfan',
-            password='test123',
-            first_name='Irfan',
-            last_name='Mulani'
-        )
-        self.assertEqual(user.first_name, 'Irfan')
-        self.assertEqual(user.last_name, 'Mulani')
+# class UserModelTest(TestCase):
+#     def test_create_user_and_check_name(self):
+#         user = User.objects.create_user(
+#             username='irfan',
+#             password='test123',
+#             first_name='Irfan',
+#             last_name='Mulani'
+#         )
+#         self.assertEqual(user.first_name, 'Irfan')
+#         self.assertEqual(user.last_name, 'Mulani')
+
+# class AccountViewTest(TestCase):
+#     def test_account_view_returns_correct_text(self):
+#         response = self.client.get(reverse('account'))  
+#         self.assertEqual(response.status_code, 200)
+#         self.assertContains(response, "Account Page")
+
