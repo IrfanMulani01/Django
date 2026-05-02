d0abd22dceb8bc9f02c580adba12388fb2fbc8a8
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-02 10:52:33 +0530

    create many to many relationship with many courses having many students

--- Django-Projects-practice/myblog/product/models.py
+++ Django-Projects-practice/myblog/product/models.py
@@ -35,3 +35,14 @@ class User_profile(models.Model):
     def __str__(self):
         return self.fname
   
+
+            ######  many - to - many realationship  #######
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    courses = models.ManyToManyField(Course)
