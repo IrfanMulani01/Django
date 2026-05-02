170a7aae15a77ad79361c9c99d81149161d25604
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-02 16:05:32 +0530

    create test case to create user and check it's name

--- Django-Projects-practice/myblog/account/tests.py
+++ Django-Projects-practice/myblog/account/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserModelTest(TestCase):
+    def test_create_user_and_check_name(self):
+        user = User.objects.create_user(
+            username='irfan',
+            password='test123',
+            first_name='Irfan',
+            last_name='Mulani'
+        )
+        self.assertEqual(user.first_name, 'Irfan')
+        self.assertEqual(user.last_name, 'Mulani')
