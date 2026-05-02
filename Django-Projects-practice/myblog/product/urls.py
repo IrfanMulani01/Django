0055943b475783d0dbd3a64abb2e24b39a981e77
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-02 15:31:40 +0530

    create dynamic urls

--- Django-Projects-practice/myblog/product/urls.py
+++ Django-Projects-practice/myblog/product/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.product),
     path('product_list/', views.product_list),
     path('prod_cat/', views.prod_cat),
+    path('student/<int:id>/', views.Student_detail),
+    path('product/<str:name>/', views.product_detail),
+    re_path(r'^article/(?P<year>[0-9]{4})/$', views.article_by_year),
 ]
