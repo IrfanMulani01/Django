52bf58ee88dfec0a937845f9606f477e5c13f4dc
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-02 15:49:56 +0530

    create test case and practice all assert functions

--- Django-Projects-practice/myblog/product/tests.py
+++ Django-Projects-practice/myblog/product/tests.py
@@ -1,5 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
 
-class SimpleText(TestCase):
-    def test_addition(self):
-        self.assertEqual(2 + 2, 3)
+# class SimpleText(TestCase):
+#     def test_addition(self):
+#         self.assertEqual(2 + 2, 4)
+
+
+# class ViewTest(TestCase):
+#     def test_home_view(self):
+#         response = self.client.get(reverse('home'))
+#         self.assertEqual(response.status_code, 200)
+#         self.assertContains(response, "welcome")
+
+
+# class CheckVal(TestCase):
+#     def test(self):
+#         self.assertNotEqual(2,2)
+
+
+# class Boolean(TestCase):
+#     def check(self):
+#         self.assertTrue(1+1, 2)
+#         self.assertFalse(1+1, 2)
+
+
+# class CheckIn(TestCase):
+#     def check(self):
+#         a=10
+#         b=a
+#         self.assertIn(a,b)
+
