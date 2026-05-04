e0b02ebd5866e5b99c29cab595032779784f8509
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 21:06:14 +0530

    create AddStudent form to check enterend age is 18 + or not

---
+++ Django-Projects-practice/models/model1/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Student
+
+class AddStudent(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['name', 'email', 'age', 'course']
+
+    def clean_age(self):
+        age = self.changed_data['age']
+        if age < 18:
+            raise forms.ValidationError("Age must be 18 or 18 + ")
+        return age
+    
