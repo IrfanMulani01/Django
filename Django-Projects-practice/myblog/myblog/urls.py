c7ea11234da98e53fb00f29b465e53829b2d2d9e
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 13:38:10 +0530

    set path of Fortemplate app urls

--- Django-Projects-practice/myblog/myblog/urls.py
+++ Django-Projects-practice/myblog/myblog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,5 +9,9 @@ urlpatterns = [
     path('product/', include('product.urls')),
     path('teacher/', include('teacher.urls')),
     path('account/', include('account.urls')),
+    path('temp/', include('Fortemplate.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
