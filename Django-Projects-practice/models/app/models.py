38a76efcefbe16033e81f8da86762db72367cba9
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 15:44:01 +0530

    create model Student with name roll_no and age

--- Django-Projects-practice/models/app/models.py
+++ Django-Projects-practice/models/app/models.py
@@ -1,11 +1,11 @@
-# from django.db import models
+from django.db import models
 
-# class Student(models.Model):
-#     name= models.CharField(max_length=100)
-#     roll_no = models.IntegerField()
-#     age = models.IntegerField()
+class Student(models.Model):
+    name= models.CharField(max_length=100)
+    roll_no = models.IntegerField()
+    age = models.IntegerField()
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
     
     
