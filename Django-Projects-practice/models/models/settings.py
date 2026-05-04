72b9ddbe7495bc87b913925f1462b7fbeb277980
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 21:34:42 +0530

    set setting of model2 app in installed apps

--- Django-Projects-practice/models/models/settings.py
+++ Django-Projects-practice/models/models/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     'app',
     'model1',
+    'model2',
 ]
 
 MIDDLEWARE = [
