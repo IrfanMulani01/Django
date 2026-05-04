0e3805f044db0c0388f20e26fe4de23b74324862
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 22:28:27 +0530

    create add_prod functionto check request method is post or not

--- Django-Projects-practice/models/model2/views.py
+++ Django-Projects-practice/models/model2/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .forms import Add_product
+from .models import Product
 
-# Create your views here.
+def add_prod(request):
+    if request.method == "POST":
+        form = Add_product(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'temp/success.html')
+    
+    else:
+        form = Add_product()
+
+    return render(request, 'temp/index.html', {'form':form})
+
+def disp_prod(request):
+    prod = Product.objects.all()
+    return render(request, 'temp/list.html', {'product': prod})
