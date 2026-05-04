4e32b7016997f62f6e70799cf0c6973073d02136
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 14:07:09 +0530

    create model student

--- Django-Projects-practice/exercies/exercies1/models.py
+++ Django-Projects-practice/exercies/exercies1/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 
-# class Student(models.Model):
-#     name = models.CharField( max_length=50)
-#     roll = models.IntegerField()
-#     email = models.EmailField( max_length=254)
+class Student(models.Model):
+    name = models.CharField( max_length=50)
+    roll = models.IntegerField()
+    email = models.EmailField( max_length=254)
+    password = models.CharField(max_length=20)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
     
