04f4d21285269bc6a96bd5455885bab984f1dc05
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 20:56:49 +0530

    create Student model with name, email, age and course and also create __str__ function to display name as object on admin pannel

--- Django-Projects-practice/models/model1/models.py
+++ Django-Projects-practice/models/model1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    age = models.IntegerField()
+    course = models.CharField( max_length=50)
+
+
+    def __str__(self):
+        return self.name
+    
