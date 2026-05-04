43276473eda00ca7cdd12539fc3e328e17bc65ae
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 15:51:38 +0530

    set path of app-base url

--- Django-Projects-practice/models/app/urls.py
+++ Django-Projects-practice/models/app/urls.py
@@ -1,6 +1,6 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# urlpatterns = [
-#     path('', views.create_post),
-# ]
+urlpatterns = [
+    path('', views.create_post),
+]
