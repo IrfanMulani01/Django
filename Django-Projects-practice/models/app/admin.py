389540c760055670f3b63050df9af99f8c94d49f
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 15:42:38 +0530

    create project models to solve the models operations

---
+++ Django-Projects-practice/models/app/admin.py
@@ -0,0 +1,11 @@
+# from django.contrib import admin
+# from .models import Student 
+
+# # class StudentAdmin(admin.ModelAdmin):
+# #     list_display = ['id', 'name', 'roll_no', 'age']
+# #     search_fields = ['name']
+# #     list_filter = ['id']
+# #     ordering = ['id']
+# #     readonly_fields = ['age']
+
+# admin.site.register(Student, StudentAdmin)
---
+++ Django-Projects-practice/models/app/views.py
@@ -0,0 +1,7 @@
+# from django.shortcuts import render
+# from .models import Student
+# from django.http import HttpResponse
+
+# def create_post(request):
+#     Student.objects.create(name = "vaibav", roll_no = 18, age = 22)
+#     return HttpResponse("Post Created")
