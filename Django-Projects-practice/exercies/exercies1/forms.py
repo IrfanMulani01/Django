1bdc40235b242c1de063eee3deeca898f4356016
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 14:06:36 +0530

    create two forms student form and student model form to check age and roll no is valid or not

---
+++ Django-Projects-practice/exercies/exercies1/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Student
+
+
+class StudenForm(forms.Form):
+    name = forms.CharField(max_length=50, required=False)
+    email = forms.EmailField()
+    age = forms.IntegerField()
+
+    def clean_age(self):
+        age = self.cleaned_data['age']
+        if age <= 18:
+            raise forms.ValidationError("age must be 18 or 18+")
+        return age
+    
+class StudentModelForm(forms.ModelForm):
+    class Meta:
+        models = Student
+        fields = ['name', 'age', 'email', 'password']
+        widget =  {
+            'password' : forms.PasswordInput()
+        }
+
+    def clean_roll(self):
+        roll = self.changed_data['roll']
+        if roll <= 0:
+            raise  forms.ValidationError("Age must be in positive")
+        return roll
