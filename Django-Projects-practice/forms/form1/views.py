019ecf5c05e3d4600f84b961afe6beae8a43a2bf
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 16:50:42 +0530

    create student_detail function to checks request method is POst or not if yes then it checks data is valid or not if yes then redirect success.html file or not then it send try again in index.html file

--- Django-Projects-practice/forms/form1/views.py
+++ Django-Projects-practice/forms/form1/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import StudentModelForm
 
-# Create your views here.
+def student_deatil(request):
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'temp/success.html')
+    else:
+        form = StudentModelForm()
+
+    return render(request, 'temp/index.html', {'form': form})
