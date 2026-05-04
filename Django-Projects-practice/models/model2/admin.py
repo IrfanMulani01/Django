8fc0acb36db58f05c04c3dd907cb6d11e26b545c
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 22:26:30 +0530

    create custom admin pannel with list_display, search_field, list_filter, and last ordering and register it on admin pannel

--- Django-Projects-practice/models/model2/admin.py
+++ Django-Projects-practice/models/model2/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+class Disp_prod(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity', 'description']
+    search_fields = ['name']
+    list_filter = ['name']
+    ordering = ['id']
+
+admin.site.register(Product, Disp_prod)
