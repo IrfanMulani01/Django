48c1232f2c0fce0d21948ca7a7463814f033fff5
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-04 21:14:14 +0530

    create add_stud function to first check reuqest method is post or not if yes then it prossed to set form request as post then check form is_valid or not if valid then it saved and render success.html file if not get post method it repeate all process and render index.html file

--- Django-Projects-practice/models/model1/views.py
+++ Django-Projects-practice/models/model1/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import AddStudent
 
-# Create your views here.
+def add_stud(request):
+    if request.method == 'POST':
+        form = AddStudent(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'temp/success.html')
+        
+    else:
+        form = AddStudent()
+
+    return render(request, 'temp/index.html', {'form':form})
