363c362c9fccc79b5b2baaf9254938abebdb35c0
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-05 11:09:52 +0530

    remove user.create_at in line 16

--- Django-Projects-practice/models/model3/views.py
+++ Django-Projects-practice/models/model3/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.template.context_processors import request
+from .forms import Add_user, Update_UserPorfile
 
-# Create your views here.
+def add_user(request):
+    if request.method == 'POST':
+        form = Add_user(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'temp/success.html')
+        
+    else:
+        form = Add_user()
+
+    return render(request, 'temp/index.html', {'form': form})
+
+
+def update_pro(request):
+    if request.method == 'POST':
+        form = Update_UserPorfile(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'temp/success.html')
+        
+    else:
+        form = Update_UserPorfile()
+    
+    return render(request, 'temp/form2.html')
