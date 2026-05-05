c7d5b15cc8047783b1649c56e68cb5ce03e63503
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-05 13:05:21 +0530

    edit bio to display bio in 4 rows anf 50 columns

--- Django-Projects-practice/models/model3/forms.py
+++ Django-Projects-practice/models/model3/forms.py
@@ -19,11 +19,14 @@ class Add_user(forms.ModelForm):
 class Update_UserPorfile(forms.ModelForm):
     class Meta:
         model = User_Profile
-        fields = ['user', 'bio', 'profile_pic']
+        fields = ['user', 'bio', 'profile_pic'] 
+        widgets = {
+            'bio': forms.Textarea(attrs={'rows': 4, 'cols': 50}),
+        }
 
 
 class User_profile(forms.Form):
     name = forms.CharField(max_length=50)
     username = forms.CharField(max_length=100)
-    profile_pic = forms.ImageField()
-    bio = forms.TextInput()
+    profile_pic = forms.ImageField(required=False)
+    bio = forms.CharField(widget=forms.Textarea(attrs={'rows': 4}))
