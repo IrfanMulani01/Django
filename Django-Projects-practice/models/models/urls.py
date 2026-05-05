d8e872e54604ea97c57f83df5065e3a14e06e39a
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-05 13:11:12 +0530

    also set the path of media folder in project-base urls

--- Django-Projects-practice/models/models/urls.py
+++ Django-Projects-practice/models/models/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -26,3 +28,5 @@ urlpatterns = [
     path('model2/', include('model2.urls')),
     path('model3/', include('model3.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
