680c76b6d7e16d99e590331d5204ae1a923b01b2
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-05 11:15:17 +0530

    create function update_pro for check request is post or not if yes then this request store in form variable then check form is valid or not if yes then it save and render success.html file if not form will re render

--- Django-Projects-practice/models/model3/views.py
+++ Django-Projects-practice/models/model3/views.py
@@ -1,28 +1,16 @@
 from django.shortcuts import render
-from django.template.context_processors import request
-from .forms import Add_user, Update_UserPorfile
-
-def add_user(request):
-    if request.method == 'POST':
-        form = Add_user(request.POST)
-        if form.is_valid():
-            form.save()
-            return render(request, 'temp/success.html')
-        
-    else:
-        form = Add_user()
-
-    return render(request, 'temp/index.html', {'form': form})
-
+from .forms import Update_UserPorfile
+from .models import User_Profile
 
 def update_pro(request):
+    obj = User_Profile.objects.get(id=1) 
     if request.method == 'POST':
-        form = Update_UserPorfile(request.POST)
+        form = Update_UserPorfile(request.POST, instance=obj)
         if form.is_valid():
             form.save()
             return render(request, 'temp/success.html')
         
     else:
-        form = Update_UserPorfile()
+        form = Update_UserPorfile(instance=obj)
     
-    return render(request, 'temp/form2.html')
+    return render(request, 'temp/index.html', {'form': form})
