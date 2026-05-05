893352332e4b79b7ea9a6c32cdb8b26938bbed6c
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-05 10:39:08 +0530

    create custom admin pannel with list_display, search_field, list_filter, ordering and register User_profile and Cust_admin to admin pannel

--- Django-Projects-practice/models/model3/admin.py
+++ Django-Projects-practice/models/model3/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User_Profile
 
-# Register your models here.
+class Cust_admin(admin.ModelAdmin):
+    list_display = ['user', 'bio', 'profile_pic', 'create_at']
+    search_fields = ['name']
+    list_filter = ['name']
+    ordering = ['id']
+
+admin.site.register(User_Profile, Cust_admin)
