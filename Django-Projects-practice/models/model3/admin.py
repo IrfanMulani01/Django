2e38b3226f889d276c21168ff50add099990e0da
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-05 11:12:27 +0530

    create second custom admin for User and resiter user to admin pannel

--- Django-Projects-practice/models/model3/admin.py
+++ Django-Projects-practice/models/model3/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import User_Profile
+from .models import User, User_Profile
 
 class Cust_admin(admin.ModelAdmin):
-    list_display = ['user', 'bio', 'profile_pic', 'create_at']
-    search_fields = ['name']
-    list_filter = ['name']
+    list_display = ['id','user', 'bio', 'profile_pic', 'create_at']
+    search_fields = ['user']
+    list_filter = ['user']
+    ordering = ['id']
+
+class Cust_admin2(admin.ModelAdmin):
+    list_display = ['id', 'name', 'username', 'password']
     ordering = ['id']
 
 admin.site.register(User_Profile, Cust_admin)
+admin.site.register(User, Cust_admin2)
