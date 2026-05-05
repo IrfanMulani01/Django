dcacdf5309c004818a9d2605c17f78b46adca43c
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-05 11:32:47 +0530

    create add_user function to add new user

--- Django-Projects-practice/models/model3/views.py
+++ Django-Projects-practice/models/model3/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from .forms import Update_UserPorfile
+from .forms import Add_user,Update_UserPorfile
 from .models import User_Profile
+
+def add_user(request):
+    if request.method == 'POST':
+        form = Add_user(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'temp/success.html')
+        
+    else:
+        form = Add_user()
+
+    return render(request, 'temp/userAdd.html', {'form': form})
+
 
 def update_pro(request):
     obj = User_Profile.objects.get(id=1) 
+    if not obj:
+        return render(request, 'temp/error.html', {'msg': 'Profile not found'})
+
     if request.method == 'POST':
         form = Update_UserPorfile(request.POST, instance=obj)
         if form.is_valid():
