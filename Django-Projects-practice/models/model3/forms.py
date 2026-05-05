c006abba214667246647d8d85725144c8ade71f3
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-05 11:10:39 +0530

    remove Add_user form

--- Django-Projects-practice/models/model3/forms.py
+++ Django-Projects-practice/models/model3/forms.py
@@ -1,22 +1,17 @@
 from django.forms import fields
 from django import forms
-from .models import User, User_Profile
-
-class Add_user(forms.ModelForm):
+from .models import User_Profile
+  
+class Update_UserPorfile(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['name', 'username', 'password', 'create_at']
+        model = User_Profile
+        fields = ['user', 'bio', 'profile_pic']
         widget = {
-            forms.PasswordInput()
+            'password' : forms.PasswordInput()
         }
 
-    def cleasn_password(self):
+    def clean_password(self):
         pas = self.cleaned_data['password']
         if len(pas) <= 6:
             raise forms.ValidationError("Password must be greater than 6 characters")
         return pas
-    
-class Update_UserPorfile(forms.ModelForm):
-    class Meta:
-        model = User_Profile
-        fields = ['user', 'bio', 'profile_pic', 'create_at']
