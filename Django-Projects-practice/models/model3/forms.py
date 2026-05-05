49a5e47c16f259fcaa9ded58f0536b1f551c88b6
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-05 10:35:01 +0530

    create two forms add_user and update_userprofile

---
+++ Django-Projects-practice/models/model3/forms.py
@@ -0,0 +1,22 @@
+from django.forms import fields
+from django import forms
+from .models import User, User_Profile
+
+class Add_user(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'password', 'create_at']
+        widget = {
+            forms.PasswordInput()
+        }
+
+    def cleasn_password(self):
+        pas = self.cleaned_data['password']
+        if len(pas) <= 6:
+            raise forms.ValidationError("Password must be greater than 6 characters")
+        return pas
+    
+class Update_UserPorfile(forms.ModelForm):
+    class Meta:
+        model = User_Profile
+        fields = ['user', 'bio', 'profile_pic', 'create_at']
